820ee8adbf4b278c67c76da654eaa967f58542f9
postolenko <denis.postolenko@gmail.com>
2017-07-31 08:44:26 +0300

    Online Fishing

--- js/scripts.js
+++ js/scripts.js
@@ -85,6 +85,14 @@ $(document).ready(function() {
 
     });
 
+    // ----------- Tel Input Mask ---------------------
+
+    $(function() {
+
+        $("input[type='tel']").mask("+7 (999) 999-99-99");
+
+    });
+
     // ----------- Sliding Box -------------------------
 
     $(function() {
